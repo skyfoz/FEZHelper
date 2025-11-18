58bc447991aede2650ba6da2c3169f488287f83f
Skyfoz <s.gourves46@gmail.com>
2025-11-18 18:00:19 +0100

    setup lonn

--- Loenn/entities/fezCenter.lua
+++ Loenn/entities/fezCenter.lua
@@ -1,1 +1,13 @@
+local fezCenter = {}
 
+fezCenter.name = "FEZHelper/fezCenter"
+fezCenter.depth = 1100;
+fezCenter.texture = "objects/FEZHelper/fezCenter/center"
+fezCenter.nodeLimits = {3, 3}
+
+
+fezCenter.placements = {
+    name = "FEZ Center",
+}
+
+return fezCenter
